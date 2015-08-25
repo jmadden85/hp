3da5aa4200642f664a02fe7c24be631f116e9e09
jmadden85 <joe@beardbro.com>
2015-08-25 00:14:36 -0400

    fixed route from html5 shutoff

--- auction/public/auction/common/routes.js
+++ auction/public/auction/common/routes.js
@@ -1,7 +1,7 @@
 auction.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
     $routeProvider
-    .when('/auction', {
+    .when('/', {
       templateUrl: '/auction/public/auction/common/home.html'
     })
     .when('/auctioneer', {
@@ -16,7 +16,7 @@ auction.config(['$routeProvider', '$locationProvider', function ($routeProvider,
       reloadOnSearch: false
     })
     .otherwise({
-      redirectTo: '/auction'
+      redirectTo: '/'
     });
 
     $locationProvider.html5Mode({
